813aa6ee7b8fe6c9b3aaf53f6e8f05aab6deeef0
ChunYen Chang <fikrusnk@gmail.com>
2019-11-01 14:48:37 +0800

    DOCS: Write documentations for each function

--- dags/lib/copy_data_from_hdfs_to_hive.py
+++ dags/lib/copy_data_from_hdfs_to_hive.py
@@ -4,6 +4,12 @@ from pyhive import hive
 
 # define functions
 def show_hive_database(hive_server_ip, hive_server_port):
+	'''
+	Description: This function helps users to check the database information in Hive
+	Parameters: -hive_server_ip: the ip address of your Hive master node
+		    -hive_server_port: the port of your Hive master node
+	Returns: None
+	'''
 	# connect to hive
 	conn = hive.connect(host=hive_server_ip, port=hive_server_port, username='root')
 	cursor = conn.cursor()
@@ -17,6 +23,14 @@ def show_hive_database(hive_server_ip, hive_server_port):
 	conn.close()
 
 def show_hive_table_data(hive_server_ip, hive_server_port, database_name, table_name):
+        '''
+        Description: This function helps user to check the table information in Hive
+        Parameters: -hive_server_ip: the ip address of your Hive master node
+                    -hive_server_port: the port of your Hive master node
+		    -database_name: the database your table resides 
+		    -table_name: the table you want to check 
+        Returns: None
+        '''
 	# connect to hive
 	conn = hive.connect(host=hive_server_ip, port=hive_server_port, username='root')
         cursor = conn.cursor()
@@ -31,6 +45,17 @@ def show_hive_table_data(hive_server_ip, hive_server_port, database_name, table_
         conn.close()
 
 def upload_data_to_hive_table(hive_server_ip, hive_server_port, database_name, table_name, previous_task_id, **kwargs):
+	'''
+        Description: This function helps users to upload data from Hdfs to Hive
+        Parameters: -hive_server_ip: the ip address of your Hive master node
+                    -hive_server_port: the port of your Hive master node
+                    -database_name: the database your table resides
+                    -table_name: the table you want to check
+		    -previous_task_id: this parameter is used for airflow Xcom.
+		     By having this parameter, this function can know which data
+		     should load into the Hive.
+        Returns: None
+        '''
         # retrive file name from previous task through Xcom
         ti = kwargs['ti']
         file_name = ti.xcom_pull(task_ids=previous_task_id)
@@ -49,6 +74,15 @@ def upload_data_to_hive_table(hive_server_ip, hive_server_port, database_name, t
         conn.close()
 
 def execute_command_in_hive(hive_server_ip, hive_server_port, *args):
+	'''
+        Description: This function gives users the freedom to execute the command they
+		     like. They only have to type the command in the *args place, and
+		     the command can be executed.
+        Parameters: -hive_server_ip: the ip address of your Hive master node
+                    -hive_server_port: the port of your Hive master node
+		    -*args: commands users want to execute
+        Returns: None
+        '''
 	# connect to hive
         conn = hive.connect(host=hive_server_ip, port=hive_server_port, username='root')
         cursor = conn.cursor()
