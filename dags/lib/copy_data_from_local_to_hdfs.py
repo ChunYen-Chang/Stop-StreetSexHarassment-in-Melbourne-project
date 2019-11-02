8778a0da5d34764d6ef01c8c22f2f77869c7fa9e
ChunYen Chang <fikrusnk@gmail.com>
2019-11-02 16:47:32 +0800

    DOCS: Write documentations for each function

--- dags/lib/copy_data_from_local_to_hdfs.py
+++ dags/lib/copy_data_from_local_to_hdfs.py
@@ -4,10 +4,25 @@ from hdfs.client import Client
 
 # define functions
 def make_directory(hdfs_address, directory_path, directory_name):
+	'''
+        Description: This function helps users to create a directory in hdfs
+        Parameters: -hdfs_address: hadoop master node ip address
+                    -directory_path: the path the user want to create a directory
+		    -directory_name: the directory name
+        Returns: None
+        '''
 	client = Client(hdfs_address)
 	client.makedirs(directory_path + directory_name)
 
 def upload_data(hdfs_address, previous_task_id, **kwargs):
+	'''
+        Description: This function helps users to upload a file from local to hdfs
+        Parameters: -hdfs_address:  hadoop master node ip address
+		    -previous_task_id: this parameter is used for airflow Xcom.
+		     By having this parameter, this function can know which data
+		     should load into hdfs.
+        Returns: None
+        '''
 	# retrive file name from previous task through Xcom 
 	ti = kwargs['ti']
 	file_name = ti.xcom_pull(task_ids=previous_task_id)	
@@ -21,6 +36,13 @@ def upload_data(hdfs_address, previous_task_id, **kwargs):
 	client.upload(hdfs_file_path, local_file_path)
 
 def list_files(hdfs_address, hdfs_file_path):
+	'''
+        Description: This function helps users to check files in a specific hdfs
+		     directory
+        Parameters: -hdfs_address: hadoop master node ip address
+                    -hdfs_file_path: the hdfs directory path 
+        Returns: None
+        '''
 	# connect to hdfs
 	client = Client(hdfs_address)
 
