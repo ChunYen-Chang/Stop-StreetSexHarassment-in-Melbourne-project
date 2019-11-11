18ee77ae75fd885ed0a8adb4be8056ed64ce2fee
ChunYen Chang <fikrusnk@gmail.com>
2019-11-12 02:53:10 +0800

    FIX: Modify list_file function

--- code/dags/lib/copy_data_from_local_to_hdfs.py
+++ code/dags/lib/copy_data_from_local_to_hdfs.py
@@ -11,7 +11,7 @@ def make_directory(hdfs_address, directory_path, directory_name):
 		    -directory_name: the directory name
         Returns: None
         '''
-	client = Client(hdfs_address)
+	client = Client('http://' + hdfs_address)
 	client.makedirs(directory_path + directory_name)
 
 def upload_data(hdfs_address, previous_task_id, **kwargs):
@@ -35,16 +35,25 @@ def upload_data(hdfs_address, previous_task_id, **kwargs):
 	# upload the file
 	client.upload(hdfs_file_path, local_file_path)
 
-def list_files(hdfs_address, hdfs_file_path):
+def list_files_in_flume_directory(hdfs_address):
 	'''
         Description: This function helps users to check files in a specific hdfs
 		     directory
         Parameters: -hdfs_address: hadoop master node ip address
                     -hdfs_file_path: the hdfs directory path 
-        Returns: None
+        Returns: files_utf8_encode. A list which contains all files' name in a 
+		 directory
         '''
 	# connect to hdfs
-	client = Client(hdfs_address)
+	client = Client('http://' + hdfs_address)
 
 	# list all folders
-	client.list(hdfs_file_path)
+	files =	client.list('/flume')
+	files_utf8_encode = map(lambda x: x.encode('utf-8'), files)
+	if len(files_utf8_encode) > 1:
+		print('Please check the data pipeline, the number of files should be 1')
+	else:
+		print(files_utf8_encode[0])
+		return files_utf8_encode[0]	
+
+
